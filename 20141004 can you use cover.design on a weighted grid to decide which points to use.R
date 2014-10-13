1fc654e8066ba2b20d96b1c06fc61dba3a39c721
Anthony Damico <ajdamico@gmail.com>
2014-10-13 14:30:22 -0400

    folder cleanup

--- 20141004 can you use cover.design on a weighted grid to decide which points to use.R
+++
@@ -1,9 +0,0 @@
-library(fields)
-data(ozone2)
-y16<- ozone2$y[16,] 
-good<- !is.na( y16)
-y<- y16[good] 
-x<- ozone2$lon.lat[ good,]
-xknots<- cover.design( x, 25)$design  # select 50 knot points
-out<- Krig( x, y, knots=xknots,  cov.function="Exp.cov", theta=1)  
-surface(out)
